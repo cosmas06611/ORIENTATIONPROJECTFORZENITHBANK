05938d2454a3d81e3305e5429d8242b01897b11a
cosmas06611 <cosmasndulue00611@gmail.com>
2026-01-03 13:51:30 +0100

    made corrections

--- src/main/java/com/cosmas/orientationapp/OrientationAppApplication.java
+++ src/main/java/com/cosmas/orientationapp/OrientationAppApplication.java
@@ -1,5 +1,6 @@
 package com.cosmas.orientationapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,6 +11,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class OrientationAppApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(OrientationAppApplication.class, args);
 	}
 
